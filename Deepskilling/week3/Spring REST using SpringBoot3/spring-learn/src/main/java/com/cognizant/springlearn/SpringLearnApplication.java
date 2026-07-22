5955b18fa7cffe11cac53c7d7f1333471d0efd1a
prachi <prachi29003@gmail.com>
2026-07-22 22:01:04 +0530

    Added Week3 Spring Boot Hands-on 1 and Hands-on 2

--- Deepskilling/week3/Spring REST using SpringBoot3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Deepskilling/week3/Spring REST using SpringBoot3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,10 +1,14 @@
 package com.cognizant.springlearn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -12,13 +16,27 @@ public class SpringLearnApplication {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(SpringLearnApplication.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        LOGGER.info("Spring Boot Application Started");
+    LOGGER.info("Spring Boot Application Started");
 
-        SpringApplication.run(SpringLearnApplication.class, args);
+    SpringApplication.run(SpringLearnApplication.class, args);
 
-        LOGGER.info("Spring Boot Application Running Successfully");
+    LOGGER.info("Spring Boot Application Running Successfully");
+
+    displayDate();
+}
+
+    public static void displayDate() throws Exception {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+
+        System.out.println("Parsed Date : " + date);
     }
-
 }
